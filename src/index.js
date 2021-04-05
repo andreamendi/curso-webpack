2a4c844635db4267afcdb6d62b49e7c3b0331d02
Dev <dev@Andrea-Mendis-iMac.local>
2021-04-05 16:52:13 -0500

    Proyecto basico de JSVanilla, usando una API de contactos, se hizo el primer build, instalamos webpack, se configuro el webpack.config.js y ahorita estamos poniendo babal. Pero tuve un error y no corre. Estoy en eso

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// import Template from './templates/Template.js';
+// console.log('hola');
+
+// (async function App() {
+//   const main = null || document.getElementById('main');
+//   main.innerHTML = await Template();
+// })();
+
 import Template from './templates/Template.js';
 console.log('hola');
 
