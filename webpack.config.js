571b667c76777711624dbac5d33ce1a9807f8fb1
Dev <dev@Andrea-Mendis-iMac.local>
2021-04-23 17:55:57 -0500

    Se arreglo el archivo de .babelrc y de webpack.config.js. en el de babelrc se presentaron varios typos que impedian que corriera el comando build

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,4 @@
 //Aquí vivira toda la configuración del proyecto
-// const path = require('path');
-
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'main.js'
-//   },
-//   resolve: {
-//     extensions: ['.js']
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.m?js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader'
-//         }
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       inject: true,
-//       template: './public/index.html',
-//       filename: './index.html'
-//     })
-//   ]
-// }
-
 
 //mjs -> modulos
 const path = require('path');
